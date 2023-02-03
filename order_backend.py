74a3ca1ae51b9c250e5a4e7f41ca0ec874dbb261
Thanh Binh <tranngocthanhbinh01@gmail.com>
2023-02-03 10:34:51 +0700

    first commit

---
+++ order_backend.py
@@ -0,0 +1,23 @@
+import json
+import time
+from kafka import KafkaProducer
+
+ORDER_KAFKA_TOPIC = "order_details"
+ORDER_LIMIT = 20_000
+
+producer = KafkaProducer(bootstrap_servers="localhost:29092")
+
+
+for i in range(1, ORDER_LIMIT):
+    data = {
+        "order_id": i,
+        "user_id": f"tom_{i}",
+        "total_cost": i * 2,
+        "items": "burger, sandwich"
+    }
+
+    producer.send(
+        ORDER_KAFKA_TOPIC,
+        json.dumps(data).encode("utf-8")
+    )
+    print(f"Done sending...{i}")
